90ce1352c25e9bdda8cc95d7b4800083050e5d0e
fuji999 <fujimoutae@gmail.com>
2026-06-08 20:53:55 +0300

    Add files via upload

--- downloads/exercise_picker.py
+++ downloads/exercise_picker.py
@@ -1,7 +1,13 @@
 # https://www.youtube.com/@MatikkamatskutTube/playlists
 # https://www.geogebra.org/classic
 
-def main(course, amount):
+# course: 0 = random, 1-13 = 1-13
+# amount: desired number of exercises
+# min: lower bound of exercises to be picked from
+# max: higher bound of exercises to be picked from
+# custom_range: 0 = random, 1 = specific range defined by min, max
+
+def main(course, amount, min, max, custom_range):
     import random;  list, exercises, string = [], [], ""
 
     material = [
@@ -9,16 +15,19 @@ def main(course, amount):
     ("MAA4 Vektorit", 301), ("MAA5 Analyyttinen geometria", 354), ("MAA6 Derivaatta", 342),
     ("MAA7 Trigonometriset funktiot", 353), ("MAA8 Juuri- ja logaritmifunktiot", 343), ("MAA9 Integraalilaskenta", 316),
     ("MAA10 Todennäköisyys ja tilastot", 327), ("MAA11 Lukuteoria ja todistaminen", 299), ("MAA12 Algoritmit matematiikassa", 301),
-    ("MAA13 Differentiaali- ja integraalilaskennan jatkokurssi", 297)
-    ]
+    ("MAA13 Differentiaali- ja integraalilaskennan jatkokurssi", 297)]
     
     if course == 0:
         selection = random.choice(material)
     if course >= 1 and course <= 13:
         selection = material[course - 1]
 
-    for i in range(1, selection[1] + 1):
-        list.append(i)
+    if custom_range == 0:
+        for i in range(1, selection[1] + 1):
+            list.append(i)
+    if custom_range == 1:
+        for i in range(min, max + 1):
+            list.append(i)
 
     random.shuffle(list)
     
@@ -32,4 +41,4 @@ def main(course, amount):
 
     print(f"\n\n\n{selection[0]}\n\n{string}\n\n\n")
 
-main(0, 5)
+main(6, 3, 22, 100, 1)
